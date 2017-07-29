b1d3e214dc1a5b5f58befcdf11ec7e6b1dc2a143
rwadhawan <ritu.wadhawan@athenasoft.in>
2017-07-29 12:46:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/reetu/dummy/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.reetu.dummy;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+import java.util.HashMap;
+
+
+public class MainActivity extends AppCompatActivity {
+    String accessToken;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Initialize Link
+        HashMap<String, String> linkInitializeOptions = new HashMap<String,String>();
+        linkInitializeOptions.put("key", "1ea0962c38cf04ed365ec3de65e410");
+        linkInitializeOptions.put("product", "auth");
+        linkInitializeOptions.put("selectAccount", "true");
+        linkInitializeOptions.put("env", "sandbox");
+        linkInitializeOptions.put("clientName", "Test App");
+        linkInitializeOptions.put("webhook", "http://requestb.in");
+        linkInitializeOptions.put("baseUrl", "https://cdn.plaid.com/link/v2/stable/link.html");
+        // If initializing Link in PATCH / update mode, also provide the public_token
+        // linkInitializeOptions.put("public_token", "PUBLIC_TOKEN")
+
+        // Generate the Link initialization URL based off of the configuration options.
+        final Uri linkInitializationUrl = generateLinkInitializationUrl(linkInitializeOptions);
+
+        // Modify Webview settings - all of these settings may not be applicable
+        // or necesscary for your integration.
+        final WebView plaidLinkWebview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = plaidLinkWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        // Initialize Link by loading the Link initiaization URL in the Webview
+        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+
+        // Override the Webview's handler for redirects
+        // Link communicates success and failure (analogous to the web's onSuccess and onExit
+        // callbacks) via redirects.
+        plaidLinkWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Parse the URL to determine if it's a special Plaid Link redirect or a request
+                // for a standard URL (typically a forgotten password or account not setup link).
+                // Handle Plaid Link redirects and open traditional pages directly in the  user's
+                // preferred browser.
+                Uri parsedUri = Uri.parse(url);
+                if (parsedUri.getScheme().equals("plaidlink")) {
+                    String action = parsedUri.getHost();
+                    HashMap<String, String> linkData = parseLinkUriData(parsedUri);
+
+                    if (action.equals("connected")) {
+
+                        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+                    } else if (action.equals("exit")) {
+
+                        plaidLinkWebview.loadUrl(linkInitializationUrl.toString());
+                    } else {
+                        Log.d("Link action detected: ", action);
+                    }
+                    // Override URL loading
+                    return true;
+                } else if (parsedUri.getScheme().equals("https") ||
+                        parsedUri.getScheme().equals("http")) {
+                    // Open in browser - this is most  typically for 'account locked' or
+                    // 'forgotten password' redirects
+                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    // Override URL loading
+                    return true;
+                } else {
+                    // Unknown case - do not override URL loading
+                    return false;
+                }
+            }
+        });
+    }
+
+    // Generate a Link initialization URL based on a set of configuration options
+    public Uri generateLinkInitializationUrl(HashMap<String,String>linkOptions) {
+        Uri.Builder builder = Uri.parse(linkOptions.get("baseUrl"))
+                .buildUpon()
+                .appendQueryParameter("isWebview", "true")
+                .appendQueryParameter("isMobile", "true");
+        for (String key : linkOptions.keySet()) {
+            if (!key.equals("baseUrl")) {
+                builder.appendQueryParameter(key, linkOptions.get(key));
+            }
+        }
+        return builder.build();
+    }
+
+    // Parse a Link redirect URL querystring into a HashMap for easy manipulation and access
+    public HashMap<String,String> parseLinkUriData(Uri linkUri) {
+        HashMap<String,String> linkData = new HashMap<String,String>();
+        for(String key : linkUri.getQueryParameterNames()) {
+            linkData.put(key, linkUri.getQueryParameter(key));
+        }
+        return linkData;
+    }
+}
